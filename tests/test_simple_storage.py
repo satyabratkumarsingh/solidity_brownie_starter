62eda34e8314539227085ec8fcf128c3a7432865
satya <satyabratsingh@gmail.com>
2022-08-09 13:03:58 +0100

    Adding test cases

--- tests/test_simple_storage.py
+++ tests/test_simple_storage.py
@@ -9,3 +9,15 @@ def test_deploy():
     expected = 5
     #Assert
     assert starting_value == expected
+    
+def test_update_storage(): 
+    #Arrange
+    local_account = accounts[0]
+    #Act
+    simple_storage = SimpleStorage.deploy({"from": local_account})
+    transaction = simple_storage.storeFavNo(250, {"from": local_account})
+    transaction.wait(1)
+    updated_value = simple_storage.getFavNo()
+    expected = 250
+    #Assert
+    assert updated_value == expected
